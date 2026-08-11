4d4ac00989143cb609165309d3c17ba071721e02
cris20070312-dev <cris20070312@gmail.com>
2026-08-11 15:11:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,138 +1,30 @@
 function register(){
+let name=document.getElementById('username').value;
+let pass=document.getElementById('password').value;
+let code=document.getElementById('code').value;
 
-let name=document.getElementById("username").value;
-let pass=document.getElementById("password").value;
-let code=document.getElementById("code").value;
-
-
-if(code!="20061218"){
-
+if(code!=="20061218"){
 message.innerHTML="邀请码错误";
-
 return;
-
 }
 
-
-localStorage.setItem(
-"user",
-JSON.stringify({
-name:name,
-pass:pass
-})
-);
-
-
-message.innerHTML="注册成功";
-
-
+localStorage.setItem("royal25_user",JSON.stringify({name,pass}));
+message.innerHTML="注册成功，欢迎加入皇家25 FC";
 }
-
-
 
 function login(){
+let data=JSON.parse(localStorage.getItem("royal25_user"));
 
-
-let data=
-JSON.parse(localStorage.getItem("user"));
-
-
-if(
-data &&
-data.name==username.value &&
-data.pass==password.value
-){
-
-
-message.innerHTML=
-"登录成功，欢迎 "+data.name;
-
-
-localStorage.setItem(
-"login",
-"yes"
-);
-
-
-document.getElementById("locked")
-.style.display="none";
-
-
-document.getElementById("board")
-.style.display="block";
-
-
-
+if(data && data.name===username.value && data.pass===password.value){
+message.innerHTML="登录成功，欢迎 "+data.name;
 }else{
-
-
-message.innerHTML=
-"账号密码错误";
-
-
+message.innerHTML="账号密码错误";
 }
-
-
 }
-
-
-
-function logout(){
-
-localStorage.removeItem("login");
-
-location.reload();
-
-}
-
-
 
 function post(){
-
-
 let div=document.createElement("div");
-
-
 div.className="post";
-
-
-div.innerHTML=
-
-"<h3>"+title.value+
-"</h3><p>"+
-content.value+
-"</p>";
-
-
-posts.appendChild(div);
-
-
-}
-
-
-
-window.onload=function(){
-
-
-document.getElementById("board")
-.style.display="none";
-
-
-if(
-localStorage.getItem("login")
-=="yes"
-){
-
-
-document.getElementById("locked")
-.style.display="none";
-
-
-document.getElementById("board")
-.style.display="block";
-
-
-}
-
-
+div.innerHTML="<h3>"+postTitle.value+"</h3><p>"+postContent.value+"</p>";
+document.getElementById("posts").appendChild(div);
 }
